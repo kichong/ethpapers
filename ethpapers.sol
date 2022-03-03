3a50af476e5a054675d6a8ddf5d0aa5986c0f7e7
"kichong" <"kichongtran1@gmail.com">
2022-03-03 15:34:09 -0800

    add remix files

---
+++ ethpapers.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts@4.4.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.4.2/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.4.2/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract ETHPapers is ERC721, ERC721URIStorage, ERC721Burnable {
+    constructor() ERC721("ETHPapers", "ETHP") {}
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
